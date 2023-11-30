2b714202098a0300ed62ef8c5c08f27803e0cd0d
MichaelSchimittDev <michaelschimittpro@gmail.com>
2023-11-30 05:22:22 -0300

    created IngredientByIdConverter

---
+++ src/main/java/com/devstarrk/tacocloud/web/IngredientByIdConverter.java
@@ -0,0 +1,27 @@
+package com.devstarrk.tacocloud.web;
+
+import java.util.Optional;
+
+import com.devstarrk.tacocloud.entities.Ingredient;
+import com.devstarrk.tacocloud.repositories.IngredientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+@Component
+public class IngredientByIdConverter implements Converter<String, Ingredient>{
+
+    private IngredientRepository ingredientRepository;
+
+    @Autowired
+    public IngredientByIdConverter(IngredientRepository ingredientRepository){
+        this.ingredientRepository = ingredientRepository;
+    }
+
+    @Override
+    public Ingredient convert(String id) {
+        Optional<Ingredient> optionalIngredient = ingredientRepository.findById(id);
+        return optionalIngredient.isPresent() ?
+                optionalIngredient.get() : null;
+    }
+
+}
