3755488adea937e0e9e05a89d6012688d84441e0
MichaelSchimittDev <michaelschimittpro@gmail.com>
2023-11-30 20:58:19 -0300

    Controller Update

--- src/main/java/com/devstarrk/tacocloud/controller/DesignTacoController.java
+++ src/main/java/com/devstarrk/tacocloud/controller/DesignTacoController.java
@@ -4,17 +4,26 @@ import com.devstarrk.tacocloud.entities.Ingredient;
 import com.devstarrk.tacocloud.entities.Ingredient.Type;
 import com.devstarrk.tacocloud.entities.Order;
 import com.devstarrk.tacocloud.entities.Taco;
+import com.devstarrk.tacocloud.entities.User;
 import com.devstarrk.tacocloud.repositories.IngredientRepository;
 import com.devstarrk.tacocloud.repositories.TacoRepository;
+import com.devstarrk.tacocloud.repositories.UserRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+import lombok.extern.slf4j.Slf4j;
 
 import javax.validation.Valid;
 
+import java.security.Principal;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -23,16 +32,20 @@ import java.util.stream.Collectors;
 @Controller
 @RequestMapping("/design")
 @SessionAttributes("order")
+@Slf4j
 public class DesignTacoController {
 
     private final IngredientRepository ingredientRepository;
 
     private TacoRepository tacoRepository;
 
+    private UserRepository userRepository;
+
     @Autowired
-    public DesignTacoController(IngredientRepository ingredientRepository, TacoRepository tacoRepository) {
+    public DesignTacoController(IngredientRepository ingredientRepository, TacoRepository tacoRepository, UserRepository userRepository) {
         this.ingredientRepository = ingredientRepository;
         this.tacoRepository = tacoRepository;
+        this.userRepository = userRepository;
     }
 
     @ModelAttribute(name = "order")
@@ -46,7 +59,9 @@ public class DesignTacoController {
     }
 
     @GetMapping
-    public String showDesignForm(Model model){
+    public String showDesignForm(Model model, Principal principal){
+        log.info("   --- Designing taco");
+
         List<Ingredient> ingredients = new ArrayList<>();
         ingredientRepository.findAll().forEach(ingredients::add);
 
@@ -55,11 +70,18 @@ public class DesignTacoController {
             model.addAttribute(type.toString().toLowerCase(), filterByType(ingredients, type));
         }
 
+        String username = principal.getName();
+        User user = userRepository.findByUsername(username);
+
+        model.addAttribute("user", user);
+
         return "design";
     }
 
     @PostMapping
     public String processDesign(@Valid Taco taco, Errors errors, @ModelAttribute Order order){
+        log.info("   --- Saving taco");
+
         if(errors.hasErrors()){
             return "design";
         }
--- src/main/java/com/devstarrk/tacocloud/controller/OrderController.java
+++ src/main/java/com/devstarrk/tacocloud/controller/OrderController.java
@@ -1,13 +1,14 @@
 package com.devstarrk.tacocloud.controller;
 
 import com.devstarrk.tacocloud.entities.Order;
-
-
+import com.devstarrk.tacocloud.entities.User;
 import com.devstarrk.tacocloud.repositories.OrderRepository;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
@@ -27,15 +28,35 @@ public class OrderController {
     }
 
     @GetMapping("/current")
-    public String orderForm(){
+    public String orderForm(@AuthenticationPrincipal User user,
+                            @ModelAttribute Order order) {
+        if (order.getDeliveryName() == null) {
+            order.setDeliveryName(user.getFullname());
+        }
+        if (order.getDeliveryStreet() == null) {
+            order.setDeliveryStreet(user.getStreet());
+        }
+        if (order.getDeliveryCity() == null) {
+            order.setDeliveryCity(user.getCity());
+        }
+        if (order.getDeliveryState() == null) {
+            order.setDeliveryState(user.getState());
+        }
+        if (order.getDeliveryZip() == null) {
+            order.setDeliveryZip(user.getZip());
+        }
+
         return "orderForm";
     }
 
     @PostMapping
-    public String processOrder(@Valid Order order, Errors errors, SessionStatus sessionStatus) {
+    public String processOrder(@Valid Order order, Errors errors, SessionStatus sessionStatus, @AuthenticationPrincipal User user) {
         if (errors.hasErrors()) {
             return "orderForm";
         }
+
+        order.setUser(user);
+
         orderRepository.save(order);
         sessionStatus.setComplete();
 
