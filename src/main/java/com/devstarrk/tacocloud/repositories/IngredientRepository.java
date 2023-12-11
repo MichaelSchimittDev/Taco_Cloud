593507053fccc3c386716070be7de1c1c0762633
MichaelSchimittDev <michaelschimittpro@gmail.com>
2023-12-11 09:57:36 -0300

    created submodule taco-cloud-parent

--- src/main/java/com/devstarrk/tacocloud/repositories/IngredientRepository.java
+++
@@ -1,7 +0,0 @@
-package com.devstarrk.tacocloud.repositories;
-
-import com.devstarrk.tacocloud.entities.Ingredient;
-import org.springframework.data.repository.CrudRepository;
-
-public interface IngredientRepository extends CrudRepository<Ingredient, String> {
-}
--- src/main/java/com/devstarrk/tacocloud/repositories/TacoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.devstarrk.tacocloud.repositories;
-
-import com.devstarrk.tacocloud.entities.Taco;
-import org.springframework.data.repository.CrudRepository;
-
-public interface TacoRepository extends CrudRepository<Taco, Long> {
-}
